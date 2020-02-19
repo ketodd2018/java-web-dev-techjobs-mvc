e229f8094215f9369c78703a7704a7c18a517326
Karen Todd <ketodd2018@gmail.com>
2020-02-19 15:38:21 -0600

    Finished

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
@@ -28,6 +28,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+//        tableChoices.put("all", JobData.findAll());
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
@@ -57,7 +58,6 @@ public class ListController {
             model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         }
         model.addAttribute("jobs", jobs);
-
         return "list-jobs";
     }
 }
